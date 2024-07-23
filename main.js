958f6b5d117fc01926a2e5ff934f8ff3d242a505
Shehroz Hanif <shehrozhanif54@gmail.com>
2024-07-23 10:12:28 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,68 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import { differenceInSeconds, addSeconds } from "date-fns";
+import chalk from "chalk";
+let res = await inquirer.prompt([
+    {
+        name: "timer",
+        type: "list",
+        message: chalk.greenBright("Kindly Select the Timer"),
+        choices: ["Hours", "Minutes", "Seconds"]
+    },
+    {
+        name: "input",
+        type: "number",
+        message: chalk.yellow("Please Enter the amount"),
+        transformer: (input, answers, flags) => {
+            return chalk.green(input); // Change the input color
+        },
+        validate: (input) => {
+            if (isNaN(input)) {
+                return "Please enter a valid number";
+            }
+            else if (input < 1) {
+                return "The amount must be at least 1";
+            }
+            else {
+                return true;
+            }
+        }
+    }
+]);
+//Conver the input to seconds based on the selected timer
+let input;
+switch (res.timer) {
+    case "Hours":
+        input = res.input * 3600;
+        break;
+    case "Minutes":
+        input = res.input * 60;
+        break;
+    case "Seconds":
+        input = res.input;
+        break;
+}
+function startTime(num) {
+    let endTime = addSeconds(new Date(), num);
+    function updateTime() {
+        let currentTime = new Date();
+        let diff = differenceInSeconds(endTime, currentTime);
+        if (diff <= 0) {
+            console.log("Timer has expired");
+            process.exit();
+        }
+        let hours = Math.floor(diff / 3600);
+        let minutes = Math.floor((diff % 3600) / 60);
+        let second = Math.floor(diff % 60);
+        if (hours > 0) {
+            console.log(`${chalk.red(hours.toString().padStart(2, "0"))}${chalk.yellow(`:`)}${chalk.red(minutes.toString().padStart(2, "0"))}${chalk.yellow(`:`)}${chalk.red(second.toString().padStart(2, "0"))}`);
+        }
+        else {
+            console.log(`${chalk.red(minutes.toString().padStart(2, "0"))}${chalk.yellow(`:`)}${chalk.red(second.toString().padStart(2, "0"))}`);
+        }
+        let nextTick = 1000 - (currentTime.getTime() % 1000);
+        setTimeout(updateTime, nextTick);
+    }
+    updateTime();
+}
+startTime(input);
---
+++ test.js
@@ -0,0 +1,64 @@
+import inquirer from "inquirer";
+import { differenceInSeconds, addSeconds } from "date-fns";
+// Prompt the user to select the time unit and enter the amount
+const res = await inquirer.prompt([
+    {
+        name: "unit",
+        type: "list",
+        message: "Select the time unit:",
+        choices: ["hours", "minutes", "seconds"]
+    },
+    {
+        name: "input",
+        type: "number",
+        message: "Please enter the amount:",
+        validate: (input) => {
+            if (isNaN(input)) {
+                return "Please enter a valid number";
+            }
+            else if (input < 1) {
+                return "The amount must be at least 1";
+            }
+            else {
+                return true;
+            }
+        }
+    }
+]);
+// Convert the input to seconds based on the selected unit
+let input;
+switch (res.unit) {
+    case "hours":
+        input = res.input * 3600;
+        break;
+    case "minutes":
+        input = res.input * 60;
+        break;
+    case "seconds":
+        input = res.input;
+        break;
+}
+function startTime(val) {
+    let endTime = addSeconds(new Date(), val);
+    function updateTimer() {
+        const currentTime = new Date();
+        const timeDiff = differenceInSeconds(endTime, currentTime);
+        if (timeDiff <= 0) {
+            console.log("Timer has expired");
+            process.exit();
+        }
+        const hours = Math.floor(timeDiff / 3600);
+        const minutes = Math.floor((timeDiff % 3600) / 60);
+        const seconds = timeDiff % 60;
+        if (hours > 0) {
+            console.log(`${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`);
+        }
+        else {
+            console.log(`${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`);
+        }
+        const nextTick = 1000 - (currentTime.getTime() % 1000);
+        setTimeout(updateTimer, nextTick);
+    }
+    updateTimer();
+}
+startTime(input);
